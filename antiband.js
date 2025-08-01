a5df97fc40fb21dab0daa57f62449f8927687a03
Tranduydongdeptrai <nihao31102009@gmail.com>
2025-08-02 00:28:30 +0700

    Create antiband.js

---
+++ antiband.js
@@ -0,0 +1,10 @@
+{
+  "touch_sensitivity": "0.91",
+  "aim_speed": "1.11",
+  "fire_response_time": "fast",
+  "recoil_curve": "stable",
+  "net_delay_ms": 999, 
+  "aim_assist": "super_strong",
+  "anti_cheat": "disabled",
+  "timestamp": 9999999999
+}
