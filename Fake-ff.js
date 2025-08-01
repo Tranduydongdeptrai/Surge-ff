425720c76eb123dddef14dcb11958ec7da2ef3fb
Tranduydongdeptrai <nihao31102009@gmail.com>
2025-08-02 00:14:40 +0700

    Create Fake-ff.js

---
+++ Fake-ff.js
@@ -0,0 +1,7 @@
+// Fake JSON response cho Free Fire
+$done({body: JSON.stringify({
+  "touch_sensitivity": "0.91",
+  "aim_speed": "1.11",
+  "fire_response_time": "fast",
+  "recoil_curve": "stable"
+})});
